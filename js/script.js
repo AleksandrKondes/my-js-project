d605fe3a50c55a898d4d1ad361ec8da3b35f49dc
Condes <sakarto@gmail.com>
2020-10-30 12:42:20 +0200

    added some simple js code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 " use strict";
 
-console.log(1);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let firstMovie = prompt('Один из последних просмотренных фильмов?', '');
+let firstScore = +prompt('На сколько оцените его?', '');
+
+let secondMovie = prompt('Один из последних просмотренных фильмов?', '');
+let secondScore = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[firstMovie] = firstScore;
+personalMovieDB.movies[secondMovie] = secondScore;
+
